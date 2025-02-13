cb317849554024b87eb6cc3a4bb8fed29bd9797c
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-13 11:47:35 -0300

    initial commit

---
+++ src/main/java/io/github/pdrotmz/book_e_comm/BookECommApplication.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.book_e_comm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookECommApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookECommApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/pdrotmz/book_e_comm/BookECommApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.pdrotmz.book_e_comm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookECommApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
