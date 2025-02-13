da5ca899b3276cf1d4bbc235d38aae4af8ec969a
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-13 16:49:30 -0300

    feat: adds a book model

---
+++ src/main/java/io/github/pdrotmz/book_e_comm/model/Book.java
@@ -0,0 +1,79 @@
+package io.github.pdrotmz.book_e_comm.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+
+    @Column(name = "name", length = 100, nullable = false, unique = true)
+    private String name;
+
+    @Column(name = "description", length = 255, nullable = true)
+    private String description;
+
+    @Column(name = "quantity", nullable = false)
+    private Integer quantity;
+
+    @Column(name = "price", nullable = false, precision = 5, scale = 2)
+    private BigDecimal price;
+
+
+    public Book() {
+    }
+
+    public Book(UUID id, String name, String description, Integer quantity, BigDecimal price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+}
