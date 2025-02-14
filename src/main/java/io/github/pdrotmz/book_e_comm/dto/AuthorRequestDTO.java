89ffdd9340eff0fe7875b0596749d783d44bdcfe
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-14 12:39:47 -0300

    feat: adds request and response DTO author

---
+++ src/main/java/io/github/pdrotmz/book_e_comm/dto/AuthorRequestDTO.java
@@ -0,0 +1,9 @@
+package io.github.pdrotmz.book_e_comm.dto;
+
+import io.github.pdrotmz.book_e_comm.model.Book;
+import jakarta.validation.constraints.NotBlank;
+
+import java.util.List;
+
+public record AuthorRequestDTO(@NotBlank String name, @NotBlank String description, List<Book> books) {
+}
---
+++ src/main/java/io/github/pdrotmz/book_e_comm/dto/AuthorResponseDTO.java
@@ -0,0 +1,9 @@
+package io.github.pdrotmz.book_e_comm.dto;
+
+import io.github.pdrotmz.book_e_comm.model.Book;
+
+import java.util.List;
+import java.util.UUID;
+
+public record AuthorResponseDTO(UUID id, String name, String description, List<Book> books) {
+}
