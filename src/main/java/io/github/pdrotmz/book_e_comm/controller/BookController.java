f7af11141df3f8d3df633534611e7afebeb6c717
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-13 23:39:49 -0300

    feat: adds others endpoints

--- src/main/java/io/github/pdrotmz/book_e_comm/controller/BookController.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/controller/BookController.java
@@ -5,10 +5,12 @@ import io.github.pdrotmz.book_e_comm.service.BookServiceImpl;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("book")
@@ -24,5 +26,35 @@ public class BookController {
     public ResponseEntity<Book> registerBook(@Valid @RequestBody Book book) {
         Book newBook = bookService.registerBook(book);
         return ResponseEntity.status(HttpStatus.CREATED).body(newBook);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Book>> findAllBooks() {
+        List<Book> books = bookService.findAllBooks();
+        return ResponseEntity.status(HttpStatus.OK).body(books);
+    }
+
+    @GetMapping("/search/id/{id}")
+    public ResponseEntity<Book> findBookById(@PathVariable UUID id) {
+        Optional<Book> bookId = bookService.findBookById(id);
+        return bookId.map(ResponseEntity::ok).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/search/name/{name}")
+    public ResponseEntity<Book> findBookByName(@PathVariable String name) {
+        Optional<Book> bookName = bookService.findBookByName(name);
+        return bookName.map(ResponseEntity::ok).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/update/id/{id}")
+    public ResponseEntity<Book> updateBookById(@PathVariable UUID id, @RequestBody Book book) {
+        Optional<Book> updatedBook = bookService.updateBookById(id, book);
+        return updatedBook.map(ResponseEntity::ok).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<Void> deleteBookById(@PathVariable UUID id) {
+        bookService.deleteBookById(id);
+        return ResponseEntity.noContent().build();
     }
 }
