0106bf0ef7a08d6e61be769c376beefb348018ee
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-13 23:40:36 -0300

    feat: adds author controller

---
+++ src/main/java/io/github/pdrotmz/book_e_comm/controller/AuthorController.java
@@ -0,0 +1,65 @@
+package io.github.pdrotmz.book_e_comm.controller;
+
+import io.github.pdrotmz.book_e_comm.model.Author;
+import io.github.pdrotmz.book_e_comm.service.AuthorServiceImpl;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("author")
+public class AuthorController {
+
+    private final AuthorServiceImpl authorService;
+
+    public AuthorController(AuthorServiceImpl authorService) {
+        this.authorService = authorService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Author> registerAuthor(@RequestBody Author author) {
+        Author newAuthor = authorService.registerAuthor(author);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newAuthor);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Author>> findAllAuthors() {
+        List<Author> authors = authorService.findAllAuthors();
+        return ResponseEntity.status(HttpStatus.OK).body(authors);
+    }
+
+    @GetMapping("/search/id/{id}")
+    public ResponseEntity<Author> findAuthorById(@PathVariable UUID id) {
+        Optional<Author> authorId = authorService.findAuthorById(id);
+        return authorId
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @GetMapping("/search/name/{name}")
+    public ResponseEntity<Author> findAUthorByName(@PathVariable String name) {
+        Optional<Author> authorName = authorService.findAuthorByName(name);
+        return authorName
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/update/id/{id}")
+    public ResponseEntity<Author> updateAuthorById(@PathVariable UUID id, @RequestBody Author author) {
+        Optional<Author> updatedAuthor = authorService.updateAuthorById(id, author);
+        return updatedAuthor
+                .map(ResponseEntity::ok)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/delete/id/{id}")
+    public ResponseEntity<Void> deleteAuthorById(@PathVariable UUID id) {
+        authorService.deleteAuthorById(id);
+        return ResponseEntity.noContent().build();
+    }
+}
