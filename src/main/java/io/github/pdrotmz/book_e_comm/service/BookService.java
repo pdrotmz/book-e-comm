1671548b67802b18ae76915a27d4fd847e606ca4
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-19 15:43:35 -0300

    refactor: adapting findAllBooks for filters

--- src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
@@ -3,15 +3,16 @@ package io.github.pdrotmz.book_e_comm.service;
 import io.github.pdrotmz.book_e_comm.dto.BookRequestDTO;
 import io.github.pdrotmz.book_e_comm.dto.BookResponseDTO;
 import io.github.pdrotmz.book_e_comm.model.Book;
+import org.springframework.data.domain.Page;
 
-import java.util.List;
+import java.math.BigDecimal;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BookService {
 
     BookResponseDTO registerBook(BookRequestDTO request);
-    List<Book> findAllBooks();
+    Page<BookResponseDTO> findAllBooks(int page, int size, String name, UUID authorId, BigDecimal minPrice, BigDecimal maxPrice);
     Optional<Book> findBookById(UUID id);
     Optional<Book> findBookByName(String name);
     Optional<Book> updateBookById(UUID id, Book book);
