66ce2317b751d69671d7399095aa9f64b55daadb
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-19 13:55:17 -0300

    feat: implementing filter exists by name and description, and invalid author data

--- src/main/java/io/github/pdrotmz/book_e_comm/service/AuthorServiceImpl.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/service/AuthorServiceImpl.java
@@ -2,13 +2,13 @@ package io.github.pdrotmz.book_e_comm.service;
 
 import io.github.pdrotmz.book_e_comm.dto.AuthorRequestDTO;
 import io.github.pdrotmz.book_e_comm.dto.AuthorResponseDTO;
+import io.github.pdrotmz.book_e_comm.exception.author.AuthorAlreadyExistsException;
+import io.github.pdrotmz.book_e_comm.exception.author.AuthorNotFoundByIdException;
+import io.github.pdrotmz.book_e_comm.exception.author.AuthorNotFoundByNameException;
+import io.github.pdrotmz.book_e_comm.exception.author.InvalidAuthorDataException;
 import io.github.pdrotmz.book_e_comm.model.Author;
 import io.github.pdrotmz.book_e_comm.repository.AuthorRepository;
-import io.github.pdrotmz.book_e_comm.repository.BookRepository;
-import jakarta.persistence.EntityNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,15 +18,23 @@ import java.util.UUID;
 public class AuthorServiceImpl implements AuthorService {
 
     private final AuthorRepository repository;
-    private final BookRepository bookRepository;
 
-    public AuthorServiceImpl(AuthorRepository repository, BookRepository bookRepository) {
+    public AuthorServiceImpl(AuthorRepository repository) {
         this.repository = repository;
-        this.bookRepository = bookRepository;
     }
 
     @Override
     public AuthorResponseDTO registerAuthor(AuthorRequestDTO request) {
+        if(request.name().isBlank() || request.description().isBlank()) {
+            throw new InvalidAuthorDataException("Invalid author data. Please check the fields.");
+        }
+
+        boolean authorExists = repository.existsByNameAndDescription(request.name(), request.description());
+
+        if(authorExists) {
+            throw new AuthorAlreadyExistsException("A author with this name and description already exists.");
+        }
+
         Author author = new Author();
         author.setName(request.name());
         author.setDescription(request.description());
@@ -44,7 +52,7 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public Optional<Author> findAuthorById(UUID id) {
         if(repository.findById(id) == null || repository.findById(id).isEmpty()) {
-            throw new EntityNotFoundException("Author was not found!");
+            throw new AuthorNotFoundByIdException(id);
         }
         return repository.findById(id);
     }
@@ -52,7 +60,7 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public Optional<Author> findAuthorByName(String name) {
         if(repository.findAuthorByName(name) == null || repository.findAuthorByName(name).isEmpty()) {
-            throw new EntityNotFoundException("Book was not found!");
+            throw new AuthorNotFoundByNameException(name);
         }
         return repository.findAuthorByName(name);
     }
@@ -65,7 +73,7 @@ public class AuthorServiceImpl implements AuthorService {
             existingAuthor.setBooks(author.getBooks());
 
             return repository.save(existingAuthor);
-        }).map(Optional::of).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        }).map(Optional::of).orElseThrow(() -> new AuthorNotFoundByIdException(id));
     }
 
     @Override
