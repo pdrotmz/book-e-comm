6d743b6e50d97a317f51126c82d18388a86e4442
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-19 13:54:48 -0300

    feat: adds filter exists by name and description

--- src/main/java/io/github/pdrotmz/book_e_comm/repository/AuthorRepository.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/repository/AuthorRepository.java
@@ -1,6 +1,7 @@
 package io.github.pdrotmz.book_e_comm.repository;
 
 import io.github.pdrotmz.book_e_comm.model.Author;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +12,6 @@ import java.util.UUID;
 public interface AuthorRepository extends JpaRepository<Author, UUID> {
 
     Optional<Author> findAuthorByName(String name);
+
+    boolean existsByNameAndDescription(@NotBlank String name, @NotBlank String description);
 }
