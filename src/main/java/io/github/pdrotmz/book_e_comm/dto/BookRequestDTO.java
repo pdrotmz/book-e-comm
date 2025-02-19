dd0d28f3b2e16e602419220b4ffb055f0609896c
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-19 13:47:46 -0300

    refactor: adds annotations and a message for null or blank fields

--- src/main/java/io/github/pdrotmz/book_e_comm/dto/BookRequestDTO.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/dto/BookRequestDTO.java
@@ -6,5 +6,8 @@ import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.UUID;
 
-public record BookRequestDTO(@NotBlank String name, @NotNull Integer quantity, @NotNull BigDecimal price, @NotNull UUID authorId) {
+public record BookRequestDTO(@NotBlank(message = "Title cannot be empty") String name,
+                             @NotNull(message = "Quantity cannot be null") Integer quantity,
+                             @NotNull(message = "Price cannot be empty") BigDecimal price,
+                             @NotNull(message = "Author's id cannot be empty") UUID authorId) {
 }
