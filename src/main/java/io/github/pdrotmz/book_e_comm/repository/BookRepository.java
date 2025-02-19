5682f2644817db2ebf0598d9a980f9ac3a08d63d
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-19 13:56:14 -0300

    feat: adds filter exists by name and author id

--- src/main/java/io/github/pdrotmz/book_e_comm/repository/BookRepository.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/repository/BookRepository.java
@@ -1,6 +1,8 @@
 package io.github.pdrotmz.book_e_comm.repository;
 
 import io.github.pdrotmz.book_e_comm.model.Book;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,6 @@ import java.util.UUID;
 public interface BookRepository extends JpaRepository<Book, UUID> {
 
     Optional<Book> findBooksByName(String name);
+
+    boolean existsByNameIgnoreCaseAndAuthorId(@NotBlank(message = "Title cannot be empty") String name, @NotNull(message = "Author's id cannot be empty") UUID authorId);
 }
