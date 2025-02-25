800fb1ac900f2485b3d0740ec6ed012f35fb273c
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-25 15:48:50 -0300

    :sparkles: feat: adds contract interface for findBook by author name, id ands publisher name

--- src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
@@ -6,13 +6,18 @@ import io.github.pdrotmz.book_e_comm.model.Book;
 import org.springframework.data.domain.Page;
 
 import java.math.BigDecimal;
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface BookService {
 
     BookResponseDTO registerBook(BookRequestDTO request);
+    List<Book> findAllBooks();
     Page<BookResponseDTO> findAllBooks(int page, int size, String name, UUID authorId, BigDecimal minPrice, BigDecimal maxPrice);
+    List<Book> findBookByAuthor(UUID authorId);
+    List<Book> findBookByAuthorName(String authorName);
+    List<Book> findBookByPublisherName(String publisherName);
     Optional<Book> findBookById(UUID id);
     Optional<Book> findBookByName(String name);
     Optional<Book> updateBookById(UUID id, Book book);
