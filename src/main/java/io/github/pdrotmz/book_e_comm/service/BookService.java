0a7e408affa5de4a1533521194b9d2b3ebcf9428
pdrotmz <pedrotomaz0210@gmail.com>
2025-02-26 13:48:09 -0300

    :sparkles: feat: adds findBookByPublicationDate interface service

--- src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
+++ src/main/java/io/github/pdrotmz/book_e_comm/service/BookService.java
@@ -1,11 +1,13 @@
 package io.github.pdrotmz.book_e_comm.service;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.github.pdrotmz.book_e_comm.dto.BookRequestDTO;
 import io.github.pdrotmz.book_e_comm.dto.BookResponseDTO;
 import io.github.pdrotmz.book_e_comm.model.Book;
 import org.springframework.data.domain.Page;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
@@ -18,6 +20,7 @@ public interface BookService {
     List<Book> findBookByAuthorId(UUID authorId);
     List<Book> findBookByAuthorName(String authorName);
     List<Book> findBookByPublisherName(String publisherName);
+    List<Book> findBookByPublicationDate(@JsonFormat(pattern = "dd-MM-yyy") LocalDate publicationDate);
     Optional<Book> findBookById(UUID id);
     Optional<Book> findBookByName(String name);
     Optional<Book> updateBookById(UUID id, Book book);
